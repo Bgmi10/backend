ed3e269d40144077f9e8f71d517d1360c113d503
subash <subashchandraboseravi45@gmail.com>
2024-07-03 02:55:41 -0700

    implemteted the chat group socket

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const { createServer } = require('http');
 const cors = require('cors');
 const connectDb = require('./db')
 const todoroutes = require('./routes/todo')
+const socketIo = require('socket.io');
+
 
 
 const app = express();
 const server = createServer(app);
 
+const io = socketIo(server )
+
 connectDb()
 
 const corsOptions = {
-  origin: 'http://localhost:3000/backendtodo', // your React app's origin
+  origin: 'http://localhost:3000/', // your React app's origin
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, // enable set cookie
   optionsSuccessStatus: 200,
@@ -24,19 +28,44 @@ app.use(express.json())
 
 app.use('/api' , todoroutes )
 
+let users = [] 
 
-// io.on('connection', (socket) => {
- 
+// //{
+//   id: id,
+//   username : xyz
+// }
 
-//   socket.on('sendMessage', (message) => {
-   
-//     io.emit('receiveMessage', message);
-//   });
 
-//   socket.on('disconnect', () => {
-//     console.log('user disconnected');
-//   });
-// });
+io.on('connection' , (socket) =>{
+  console.log('new connection client with id ' , socket.id)
+
+  socket.on('join', (username) =>{
+    users.push({id: socket.id , username : username} )
+
+    io.emit('users' , users)
+  })
+
+  socket.on('message' , (message) =>{
+    io.emit('message' , message)
+  })
+
+  socket.on('typing' , (username) => {
+    socket.broadcast.emit('typing' , username)
+  })
+
+
+  socket.on('disconnect' , ()=>{
+    users = users.filter((i) => i.id !== socket.id)
+
+    io.emit('users' , users)
+
+    console.log('user disconnected' , socket.id)
+  })
+
+
+
+
+})
 
 server.listen(3001, () => {
   console.log('server running at http://localhost:3001');
